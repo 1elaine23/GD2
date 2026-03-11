b046a447e34ac9da9255a7ec5b412b5e1b9a0fe9
1elaine23 <elaineyang@ocadu.ca>
2026-03-11 17:54:44 -0400

    workshop 5 in class

---
+++ w05/js/script.js
@@ -0,0 +1,34 @@
+/* $("#preamble-btn-1").click(function(){
+    $("#preamble-btn-2").css({
+        "background":"blue",
+        "font-size":"20px",
+    });
+})
+
+
+
+$("#preamble-btn-2").click(function(){
+    $("#preamble-btn-1").toggleClass("rotate")
+}) */
+
+$(".header-left").click(function(){
+    $("#menu").toggleClass("active")
+})
+
+$(document).scroll(function(){
+    let distance = $(document).scrollTop()
+    let height = $(document).height()-$(window).height();
+    let ratio = distance/height;
+
+    $(".header-right").html(ratio)
+
+   /* $("h1").css({
+        "transform":"rotate(300deg)"
+    }) */
+})
+
+$(".album-thumbs").flickity({
+    wrapAround: true,
+    fade: true,
+    pageDots: false
+})
